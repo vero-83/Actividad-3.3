c45f6e299e0977bcba096ba9371319456282cc9f
marc-85 <marcbig85@gmail.com>
2022-10-24 20:52:13 +0200

    Added a python script to visualize output

---
+++ generate_images.py
@@ -0,0 +1,36 @@
+from array import array
+import matplotlib.pyplot as plt
+import csv
+import numpy as np
+
+X = []
+Uinit = []
+Ufinal = []
+
+# opening the CSV file
+with open('initialCondition.csv', mode ='r')as file:
+   
+  # reading the CSV file
+  csvFile = csv.reader(file)
+ 
+  # displaying the contents of the CSV file
+  for lines in csvFile:
+        X.append(float(lines[0]))
+        Uinit.append(float(lines[1]))
+
+
+with open('final.csv', mode ='r')as file:
+   
+  # reading the CSV file
+  csvFile = csv.reader(file)
+ 
+  # displaying the contents of the CSV file
+  for lines in csvFile:
+        Ufinal.append(float(lines[1]))
+        
+#print(X, Uinit)
+plt.plot(X, Uinit, label="Init")
+plt.plot(X, Ufinal, label="Final")
+plt.legend()
+plt.grid()
+plt.show()
